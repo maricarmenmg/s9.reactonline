a72d608f9642af34109cca9e3cc93cd512b42cc6
Maria del Carmen Martín Gómez <maricarmenmartin@iMac-de-Maria.local>
2023-08-10 13:13:35 +0200

    search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { FilmIcon, TvIcon, MagnifyingGlassCircleIcon} from '@heroicons/react/24/
 
 function Header() {
   return (
-    <header className="absolute w-full z-30">
+    <header className="fixed  w-full z-30 bg-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Site branding */}
